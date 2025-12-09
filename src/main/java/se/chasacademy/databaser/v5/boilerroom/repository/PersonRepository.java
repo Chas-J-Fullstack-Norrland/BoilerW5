bc502af50209a308813fa2655102ae90222a0ad2
nicolaslovgren92 <nicolas.lovgren92@gmail.com>
2025-12-09 10:44:36 +0100

    PersonRepository

--- src/main/java/se/chasacademy/databaser/v5/boilerroom/repository/PersonRepository.java
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/repository/PersonRepository.java
@@ -14,28 +14,22 @@ import java.util.Optional;
 public class PersonRepository {
 
 
-    private JdbcClient jdbcClient;
+        private final JdbcClient jdbcClient;
 
+        public PersonRepository(JdbcClient jdbcClient) {
+            this.jdbcClient = jdbcClient;
+        }
 
+        public List<Person> findAll() {
+            return jdbcClient.sql("SELECT personid, personname, adress, phonenumber FROM person")
+                    .query(Person.class)
+                    .list();
+        }
 
-    private final PersonRowMapper personRowMapper = new PersonRowMapper();
-
-    // Hämta alla personer
-    public List<Person> findAll() {
-        return jdbcClient.sql("SELECT personid, personname, adress, phonenumber FROM person")
-                .query(Person.class)  // magi – mappar direkt till record!
-                .list();
+        public Optional<Person> findById(int id) {
+            return jdbcClient.sql("SELECT personid, personname, adress, phonenumber FROM person WHERE personid = :id")
+                    .param("id", id)
+                    .query(Person.class)
+                    .optional();
+        }
     }
-
-
-    // Hämta efter ID
-    public Optional<Person> findById(int id) {
-        return jdbcClient.sql("SELECT personid, personname, adress, phonenumber FROM person WHERE person_id = :id")
-                .param("id", id)
-                .query(Person.class)
-                .optional();
-    }
-
-
-
-}
