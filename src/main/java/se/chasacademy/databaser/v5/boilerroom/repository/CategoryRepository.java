ca386e73f25dcadb7687b42aba5ff4259e83f89c
Joso1207 <joso1207@student.miun.se>
2025-12-09 09:27:24 +0100

    Repository,RowMapper and Model for Category

--- src/main/java/se/chasacademy/databaser/v5/boilerroom/repository/CategoryRepository.java
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/repository/CategoryRepository.java
@@ -1,7 +1,13 @@
 package se.chasacademy.databaser.v5.boilerroom.repository;
 
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import se.chasacademy.databaser.v5.boilerroom.models.Category;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Repository
 public class CategoryRepository {
@@ -9,6 +15,17 @@ public class CategoryRepository {
 
     public CategoryRepository(JdbcClient jdbcClient){
         this.jdbcClient = jdbcClient;
+    }
+
+    public List<Map<String, Object>> CountBooksPerCategoryAll(){
+
+        return jdbcClient.sql(
+                "Select categoryType,Count(*) as total " +
+                "from categoryJunc" +
+                "Inner join category on category.categoryid = categoryjunc.categoryid" +
+                "order by categoryType")
+                .query().listOfRows();
+
     }
 
 
